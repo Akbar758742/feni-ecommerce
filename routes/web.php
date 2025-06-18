82e4a5f5f5dada58f1db01525c9086b41d2a5da6
akbar hossen <mdakbar758742@gmail.com>
2025-06-18 20:54:49 +0600

    dashboardloginauth

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\FrontendController;
+use App\Http\Controllers\Backend\DashboardController    ;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[FrontendController::class,'index'])->name('index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/product-details', [FrontendController::class,'productDetails'])->name('product.details');
+Route::get('/dashboard', [DashboardController::class,'dashboard'])->middleware(['auth'])->name('dashboard');
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
